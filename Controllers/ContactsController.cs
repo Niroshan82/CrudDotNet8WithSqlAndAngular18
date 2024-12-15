b213b66fc49669048045b2b93acd0d20626cfb7a
Niroshan <knniroshan82@outlook.com>
2024-12-15 13:41:22 +0530

    Add project files.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,59 @@
+﻿using CrudNet8.Data;
+using CrudNet8.Models;
+using CrudNet8.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudNet8.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactlyDbContext dbContext;
+
+        public ContactsController(ContactlyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            var contacts = dbContext.Contacts.ToList();
+            return Ok(contacts);
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(AddContactRequestDTO request)
+        {
+            var AddDomainModel = new Contact()
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Favorite = request.Favorite,
+            };
+
+            dbContext.Contacts.Add(AddDomainModel); 
+            dbContext.SaveChanges();
+
+            return Ok(AddDomainModel);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult DeleteContact(Guid id)
+        {
+            var contact = dbContext.Contacts.Find(id);
+
+            if (contact is not null)
+            {
+                dbContext.Contacts.Remove(contact);
+                dbContext.SaveChanges();
+            }
+
+            return Ok();
+        }
+    }
+}
